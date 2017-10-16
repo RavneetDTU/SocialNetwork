fb46543e5a0cf5c42203ad47bc82a8c25ea2e8ad
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-10-16 22:23:40 +0530

    College Notification Added

---
+++ app/src/main/java/in/ravneet/xendra/CollegeNotification.java
@@ -0,0 +1,13 @@
+package in.ravneet.xendra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CollegeNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_notification);
+    }
+}
